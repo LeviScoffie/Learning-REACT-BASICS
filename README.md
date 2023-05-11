e5126fb189257469ca351f523b1c686a38708e98
LeviScoffie <kithinjimiriti4@gmail.com>
2023-05-11 16:22:17 +0300

    createElement

---
+++ README.md
@@ -0,0 +1,20 @@
+## REACT BASICS 
+
+- React creates plain JS objects that describe DOM nodes and not updating the real DOM
+
+- `ReactDOM.render()` -- renders react elements to the DOM.
+    - Accepts:
+        - React element/object to render 
+        - Actual HTML element you want to update or render to.
+
+
+- Any exisitng DOM elements inside the root dic will be replaced when render is called.
+
+- REACT just manages what gets updated in the page via REACTDOM.render().
+
+
+### JSX 
+
+- Redudant createElement() is replaced by JSX.
+
+- JSX is an extension to the JavaScript language that uses a markup-like syntax to create React elements. Most React developers write their UI using JSX because it resembles writing HTML.
--- app.js
+++ app.js
@@ -1,105 +1,31 @@
-const Header = (props) => {
-  return (
-    <header>
-      <span className="stats">Players: {props.totalPlayers}</span> 
-      <h1>{ props.title }</h1>
-    </header>
-  );
-}
 
-class Counter extends React.Component {
-  
-  state = { 
-    score: 100 
-  };
-  
-  incrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score + 1 
-    }));
-  }
-  
-  decrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score - 1 
-    }));
-  }
+/** This react element is an object describing what you want to render
+ * to the DOM or the display on the Screen
+ * When the h1 gets render to the DOM we get something like this  
+ * <h1 id="main-title" title="This is a title">My First React Element!</h1>
+ *  */ 
+ 
+const  title = React.createElement(
+    "h1", //element
+    {id: 'main-title', title: 'This is a title.'}, //attributes/PROPERTIES
+    'My first React Element!' //children on the elements
+)
 
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
-        <span className="counter-score"> {this.state.score} </span>
-        <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
-      </div>    
-    );
-  }
-}
-  
-const Player= (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
+const desc = React.createElement(
+    'p',
+    null,
+    "I just learned how to create a react node and render it into the dom"
 
-      <Counter />
-    </div>
-  );
-}
+);
 
-class App extends React.Component {
-  
-  state = {
-    players: [
-      {
-        name: "Guil",
-        id: 1,
-      },
-      {
-        name: "Treasure",
-        id: 2,    
-      },
-      {
-        name: "Ashley",
-        id: 3,    
-      },
-      {
-        name: "James",
-        id: 4,    
-      }
-    ]
-  };
-        
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
-      return {
-        players: prevState.players.filter(p => p.id !== id)
-      }
-    });
-  }
-  
-  render() {
-    return (
-      <div className="scoreboard">
-        <Header 
-          title="My Scoreboard" 
-          totalPlayers={this.state.players.length} 
-        />
-        
-        {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
-            removePlayer={ this.handleRemovePlayer }      
-            name={player.name}
-            id={player.id}
-            key={player.id.toString()}
-          />
-        )}  
-      </div>
-    );
-  }
-}  
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
